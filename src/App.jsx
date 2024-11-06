7c25de35839a452191504ed38520b42782976a5d
Arezoo <arezooayoblo@gmail.com>
2024-11-06 12:30:22 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+const App=()=>{
+  return (
+    <>
+      <div className="flex  justify-center h-full items-center">
+          <div className="bg-black h-12 w-12"></div>
+      </div>
+    </>
+  )
+}
+
+export default App;
